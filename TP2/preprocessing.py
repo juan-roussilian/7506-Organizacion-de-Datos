64452b91aca1d6606314da1c9c95d63ec155aa1a
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2021-12-04 16:47:31 -0300

    Cambios en el feature engineering general.

--- TP2/preprocessing.py
+++ TP2/preprocessing.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.impute import KNNImputer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer, IterativeImputer
 
 def traer_datasets():
     df_1 = pd.read_csv(
@@ -24,17 +25,23 @@ def traer_datasets():
 
 
 def separar_dataset(x, y):
-    return train_test_split(x, y, test_size=0.1, random_state=0, stratify=y)
+    X_train, X_test, y_train, y_test= train_test_split(x, y, test_size=0.1, random_state=0, stratify=y)
+    return X_train, X_test, y_train, y_test
 
 def aplicar_dummy_variables_encoding(df, columnas):
-    df_encodeado = pd.get_dummies(df, columns=columnas, dummy_na=True, drop_first=True)
+    df_encodeado = pd.get_dummies(df, columns=columnas, dummy_na=False, drop_first=True)
     return df_encodeado
 
 def aplicar_ordinal_encoding(df, columnas):
     oe = OrdinalEncoder(dtype='int')
     return oe.fit_transform(df[columnas])
     
-def feature_engineering_general(X_train, X_test):    
+def feature_engineering_general(df_train, df_test):
+    X_train = df_train.copy(deep=True)
+    X_test = df_test.copy(deep=True)
+    
+    X_train.fillna(np.nan, inplace = True)
+    X_test.fillna(np.nan, inplace = True)
     media_temp_max = X_train['temp_max'].mean()
     media_temp_min = X_train['temp_min'].mean()
     media_temp_temprano = X_train['temperatura_temprano'].mean()
@@ -45,11 +52,13 @@ def feature_engineering_general(X_train, X_test):
     X_train['velocidad_viendo_temprano'].replace(np.nan, media_vel_viento_temprano , inplace = True)
     
     X_train['presion_atmosferica_tarde'].replace('.+\..+\..+', np.nan, inplace=True, regex=True)
-    X_train['presion_atmosferica_tarde'] = pd.to_numeric(X_train['presion_atmosferica_tarde'])
+    X_train.astype({'presion_atmosferica_tarde': 'float64'}).dtypes
+    #X_train['presion_atmosferica_tarde'] = pd.to_numeric(X_train['presion_atmosferica_tarde'])
     
     eliminar_features_categoricas_general(X_train)
+    X_train.reset_index()
     X_train = aplicar_dummy_variables_encoding(X_train,['llovieron_hamburguesas_hoy'])
-    imputar_missings_KNN(X_train,5)
+    imputar_missings_KNN(X_train)
     
     X_test['temp_max'].replace(np.nan, media_temp_max , inplace = True)
     X_test['temp_min'].replace(np.nan, media_temp_min, inplace = True)
@@ -57,18 +66,21 @@ def feature_engineering_general(X_train, X_test):
     X_test['velocidad_viendo_temprano'].replace(np.nan, media_vel_viento_temprano, inplace = True)
     
     X_test['presion_atmosferica_tarde'].replace('.+\..+\..+', np.nan, inplace=True, regex=True)
-    X_test['presion_atmosferica_tarde'] = pd.to_numeric(X_test['presion_atmosferica_tarde'])
+    X_test.astype({'presion_atmosferica_tarde': 'float64'}).dtypes
+    #X_test['presion_atmosferica_tarde'] = pd.to_numeric(X_test['presion_atmosferica_tarde'])
     
     eliminar_features_categoricas_general(X_test)
+    X_test.reset_index()
     X_test = aplicar_dummy_variables_encoding(X_test, ['llovieron_hamburguesas_hoy'])
-    imputar_missings_KNN(X_test,5)
+    imputar_missings_KNN(X_test)
+    return X_train, X_test
     
     
 def eliminar_features_categoricas_general(df):
     df.drop(['id','dia','barrio', 'direccion_viento_tarde', 'direccion_viento_temprano', 'rafaga_viento_max_direccion'], axis=1, inplace=True)
 
-def imputar_missings_KNN(df, vecinos):
-    imputer = KNNImputer(vecinos, weights="uniform")
+def imputar_missings_KNN(df):
+    imputer = KNNImputer(n_neighbors = 3, weights="uniform")
     imputer.fit_transform(df)
     
 def categorizar_humedad(humedad):
