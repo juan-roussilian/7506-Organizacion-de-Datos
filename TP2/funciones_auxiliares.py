90186875d80f858057a146edcfe8f8df0213f46b
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2021-12-05 16:57:34 -0300

    Agrego Random Grid search para encontrar hiperparámetros.

---
+++ TP2/funciones_auxiliares.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+
+def encontrar_hiperparametros_RGSCV(clf, params, X, y):
+    x_np = X.to_numpy()
+    y_np = y.to_numpy()
+    rgscv = RandomizedSearchCV(clf, params, n_iter=100, scoring='roc_auc', n_jobs=-1, return_train_score=True).fit(x_np, y_np)
+    return rgscv.best_params_
