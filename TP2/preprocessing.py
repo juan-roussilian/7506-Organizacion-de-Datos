9ba7d4850bfc34a9b2a9b2d78404fef4d1bc25d3
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2021-12-06 00:53:18 -0300

    Muevo todas las funciones de traer datasets al archivo funciones_auxiliares.py

--- TP2/preprocessing.py
+++ TP2/preprocessing.py
@@ -4,36 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import KNNImputer, IterativeImputer
 
-def traer_datasets():
-    df_1 = pd.read_csv(
-        'https://docs.google.com/spreadsheets/d/1gvZ03uAL6THwd04Y98GtIj6SeAHiKyQY5UisuuyFSUs/export?format=csv', low_memory=False
-    )
-    df_2 = pd.read_csv(
-        'https://docs.google.com/spreadsheets/d/1wduqo5WyYmCpaGnE81sLNGU0VSodIekMfpmEwU0fGqs/export?format=csv', low_memory=False
-    )
-    pd.options.display.max_columns = None
-
-    df = df_2.merge(df_1, left_on = 'id', right_on = 'id')
-    df.sort_values(by=['id'], inplace=True, ascending=True)
-    df = df.dropna(subset=['llovieron_hamburguesas_al_dia_siguiente'])
-
-    
-    df_sin_target = df.copy().drop('llovieron_hamburguesas_al_dia_siguiente', 1)
-    solo_target = df[['id','llovieron_hamburguesas_al_dia_siguiente']].copy()
-    
-    return df, df_sin_target, solo_target
-
-def traer_dataset_prediccion_final():
-    df = pd.read_csv('https://docs.google.com/spreadsheets/d/1mR_JNN0-ceiB5qV42Ff9hznz0HtWaoPF3B9zNGoNPY8/export?format=csv', low_memory=False)
-    df.sort_values(by=['id'], inplace=True, ascending=True)
-    df.set_index('id', inplace=True)
-    return df
-
-
-def separar_dataset(x, y):
-    X_train, X_test, y_train, y_test= train_test_split(x, y, test_size=0.1, random_state=0, stratify=y['llovieron_hamburguesas_al_dia_siguiente'])
-    return X_train, X_test, y_train, y_test
-
 def aplicar_dummy_variables_encoding(df, columnas):
     df_encodeado = pd.get_dummies(df, columns=columnas, dummy_na=True, drop_first=True)
     return df_encodeado
