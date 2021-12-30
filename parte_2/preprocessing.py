879fa8aebf0132c3817ec1f4266fbbf7a2997887
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2021-12-30 12:27:32 -0300

    Cambio la forma en la que se imputan los datos.
    De esta forma, me aseguro de poder imputar X_test con un imputer entrenado en X_train.

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -6,6 +6,8 @@ from sklearn.impute import KNNImputer, IterativeImputer
 from sklearn.manifold import TSNE, MDS
 from sklearn.preprocessing import StandardScaler
 
+features_continuas = ['id','horas_de_sol','humedad_tarde', 'humedad_temprano', 'mm_evaporados_agua', 'mm_lluvia_dia', 'nubosidad_tarde', 'nubosidad_temprano', 'presion_atmosferica_tarde', 'presion_atmosferica_temprano', 'rafaga_viento_max_velocidad','temp_max', 'temp_min', 'temperatura_tarde', 'temperatura_temprano',  'velocidad_viendo_tarde','velocidad_viendo_temprano']
+
 def aplicar_dummy_variables_encoding(df, columnas):
     df_encodeado = pd.get_dummies(df, columns=columnas, dummy_na=True, drop_first=True)
     return df_encodeado
@@ -14,11 +16,9 @@ def aplicar_ordinal_encoding(df, columnas):
     oe = OrdinalEncoder(dtype='int')
     return oe.fit_transform(df[columnas])
 
-def feature_engineering_basico(df_a_preprocesar, features_eliminables, dummy_llovieron_hamburguesas_hoy):
+def feature_engineering_basico(df_a_preprocesar, features_eliminables, imputer, dummy_llovieron_hamburguesas_hoy):
     df = df_a_preprocesar.copy(deep=True)
     df.fillna(np.nan, inplace = True)
-    
-    features_continuas = ['id','horas_de_sol','humedad_tarde', 'humedad_temprano', 'mm_evaporados_agua', 'mm_lluvia_dia', 'nubosidad_tarde', 'nubosidad_temprano', 'presion_atmosferica_tarde', 'presion_atmosferica_temprano', 'rafaga_viento_max_velocidad','temp_max', 'temp_min', 'temperatura_tarde', 'temperatura_temprano',  'velocidad_viendo_tarde','velocidad_viendo_temprano'] 
     
     if(dummy_llovieron_hamburguesas_hoy):
         df = aplicar_dummy_variables_encoding(df, ['llovieron_hamburguesas_hoy'])
@@ -29,33 +29,44 @@ def feature_engineering_basico(df_a_preprocesar, features_eliminables, dummy_llo
     df.astype({'presion_atmosferica_tarde': 'float64'}).dtypes
     eliminar_features(df, features_eliminables)
         
-    df = imputar_missings_iterative(df, features_continuas)
+    df = imputar_missings_iterative(df, imputer)
     df.reset_index()
     df.sort_values(by=['id'], inplace=True, ascending=True)
     return df
 
-def preprocesamiento_GNB(dataframes):
+def preprocesamiento_GNB(dataframes, imputer=null):
     dataframes_procesados = []
+    
+    if(imputer == null):
+        dataframes[0].fillna(np.nan, inplace = True)
+        imputer = entrenar_iterative_imputer(dataframes[0])
+    
     for df in dataframes:
         df_procesado = feature_engineering_basico(
             df, 
             ['dia','barrio', 'direccion_viento_tarde', 'direccion_viento_temprano', 'rafaga_viento_max_direccion', 'llovieron_hamburguesas_hoy'], 
+            imputer,
             dummy_llovieron_hamburguesas_hoy=False
         )
         dataframes_procesados.append(df_procesado)
-    return dataframes_procesados
+    return dataframes_procesados, imputer
     
     
-def preprocesamiento_basico(dataframes):
+def preprocesamiento_basico(dataframes, imputer=null):
     dataframes_procesados = []
+    
+    if(imputer == null):
+        imputer = entrenar_iterative_imputer(dataframes[0])
+    
     for df in dataframes:
         df_procesado = feature_engineering_basico(
             df, 
             ['dia','barrio', 'direccion_viento_tarde', 'direccion_viento_temprano', 'rafaga_viento_max_direccion'],
+            imputer,
             dummy_llovieron_hamburguesas_hoy=True
         )
         dataframes_procesados.append(df_procesado)
-    return dataframes_procesados
+    return dataframes_procesados, imputer
 
 def normalizar_datos_standard(df_train):
     df_a_normalizar = df_train.copy()
@@ -86,10 +97,14 @@ def imputar_missings_KNN(df):
     imputer = KNNImputer(n_neighbors = 3, weights="uniform")
     imputer.fit_transform(df)
     
-def imputar_missings_iterative(df, columnas_continuas):
+def entrenar_iterative_imputer(df_train):
     imputer = IterativeImputer()
-    array_imputeado = imputer.fit_transform(df[columnas_continuas])
-    df_imputeado = pd.DataFrame(array_imputeado, columns=columnas_continuas)
+    imputer.fit(df_train[FEATURES_CONTINUAS])
+    return imputer
+    
+def imputar_missings_iterative(df, imputer_entrenado):
+    array_imputeado = imputer_entrenado.transform(df[FEATURES_CONTINUAS])
+    df_imputeado = pd.DataFrame(array_imputeado, columns=FEATURES_CONTINUAS)
     df_imputeado.set_index('id', inplace=True)
     df_imputeado = df_imputeado.sort_values('id')
     return df_imputeado
