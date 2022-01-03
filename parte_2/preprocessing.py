4fddc9f47d3abc20da66905cb8c795a779daf48d
Juan Cruz <juancruzroussilian9@gmail.com>
2022-01-02 23:08:31 -0300

    Modifico metodo del imputer para que trabaje sobre todo el dataset que recibe
    Tambien hago copia en hashing trick

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -8,21 +8,25 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.feature_extraction import FeatureHasher
 
-features_continuas = ['id','horas_de_sol','humedad_tarde', 'humedad_temprano', 'mm_evaporados_agua', 'mm_lluvia_dia', 'nubosidad_tarde', 'nubosidad_temprano', 'presion_atmosferica_tarde', 'presion_atmosferica_temprano', 'rafaga_viento_max_velocidad','temp_max', 'temp_min', 'temperatura_tarde', 'temperatura_temprano',  'velocidad_viendo_tarde','velocidad_viendo_temprano']
 
 def aplicar_dummy_variables_encoding(df, columnas):
     df_encodeado = pd.get_dummies(df, columns=columnas, dummy_na=True, drop_first=True)
     return df_encodeado
     
-def aplicar_hashing_trick(df, columnas, nuevas_features):
-    df.reset_index(inplace=True)
+def aplicar_hashing_trick(df, columnas, nuevas_features, reset_index=True):
+    df_hasheado = df.copy(deep=True)
+    
+    if reset_index:
+        df_hasheado.reset_index(inplace=True)
+        
     for columna, n_nuevas_features in zip(columnas, nuevas_features):
         hasher = FeatureHasher(n_features=n_nuevas_features, input_type='string')
-        features_hashadas = hasher.fit_transform(df[columna].astype(str)).todense()
+        features_hashadas = hasher.fit_transform(df_hasheado[columna].astype(str)).todense()
         features_hashadas = pd.DataFrame(features_hashadas).add_prefix(columna+'_')
-        df = df.join(features_hashadas)
-        df = df.drop(columna, axis=1)
-    return df
+        df_hasheado = df_hasheado.join(features_hashadas)
+        df_hasheado = df_hasheado.drop(columna, axis=1)
+
+    return df_hasheado
 
 def entrenar_normalizador_standard(df_train):
     df_a_normalizar = df_train.copy()
@@ -69,12 +73,12 @@ def imputar_missings_KNN(df):
     
 def entrenar_iterative_imputer(df_train):
     imputer = IterativeImputer()
-    imputer.fit(df_train[features_continuas])
+    imputer.fit(df_train)
     return imputer
     
 def imputar_missings_iterative(df, imputer_entrenado):
-    array_imputeado = imputer_entrenado.transform(df[features_continuas])
-    df_imputeado = pd.DataFrame(array_imputeado, columns=features_continuas)
+    array_imputeado = imputer_entrenado.transform(df)
+    df_imputeado = pd.DataFrame(array_imputeado, columns=df.columns)
     df_imputeado.set_index('id', inplace=True)
     df_imputeado = df_imputeado.sort_values('id') 
     return df_imputeado
